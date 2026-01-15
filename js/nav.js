a13adcc98c19f1122f87cd46a33f0fd9b18005c2
junaidmemon1 <junaidmushtaquememon@gmail.com>
2026-01-15 10:00:00 +0500

    feat: active section highlighting via IntersectionObserver

--- js/nav.js
+++ js/nav.js
@@ -135,3 +135,4 @@
 /* history.pushState: URL updated without scroll trigger - 2026-04-30 */
 /* nav.js v1: burger open/close, aria-expanded - 2026-01-14 */
 /* smooth scroll: NAV_HEIGHT=64 offset applied - 2026-01-14 */
+/* IntersectionObserver: active nav-link class toggled - 2026-01-15 */
